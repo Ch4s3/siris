330c71f9f0e785c093081bb7a895199c4bd0ce5b
Chase <chase.gilliam@gmail.com>
2020-04-16 18:55:43 -0400

    change config

--- config/releases.exs
+++ config/releases.exs
@@ -3,6 +3,7 @@ import Config
 secret_key_base = System.fetch_env!("SECRET_KEY_BASE")
 port = String.to_integer(System.get_env("PORT", "4000"))
 scout_key = System.get_env("SCOUT_KEY")
+dashboard_key = System.get_env("DASHBOARD_KEY")
 
 database_url =
   System.get_env("DATABASE_URL") ||
@@ -23,4 +24,4 @@ config :siris, SirisWeb.Endpoint,
 config :scout_apm,
   key: scout_key
 
-config :live_dashboard, key: System.get_env("DASHBOARD_KEY")
+config :live_dashboard, key: dashboard_key
